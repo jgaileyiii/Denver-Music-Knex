0e11a2427582d5d2ca729c66a6e54da8063241c7
Joe Gailey <jgaileyiii@gmail.com>
2019-10-25 11:37:16 -0600

    finalpush.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Mod3-Project
+
+# Denver Music KNEX: The Denver Music Teacher APP
+Interest in learning a new instrument? Look no further! Denver Music KNEX helps aspiring musicians connect to teachers around the Denver metro area. The app also allows teachers to signup and advertise.
+## Technologies
+  - Node.js - version 2.6.1
+  - Javascript - version ES9
+  - Knex.js 
+  - Express.js
+  - Nodemon 
+  - PostgreSQL
+  - Mocha
+  - Chai 
+  - Supertest
+## DEMO
+[Denver Music KNEX Demo](https://youtu.be/14aN8roQP78)
+## INSTALL INSTRUCTIONS 
+Fork and clone the repo to install it locally. Bundle install to acquire the appropriate gems. To set up the database, run:
+```javascript
+npm intall knex 
+npm install express 
+npm install nodemon
+```
+In the back end run npm run dev to start your local server. (Make sure your ports match). 
+```javascript
+(Opt: PORT=####) rails s
+```
+In the front end install lite-server:
+```javascript
+npm install lite-server
+```
+Run lite-server by typing lite-server in your terminal on the front end.
+## Features
+ - View Teachers for specific instruments. See their styles and a short Bio. Links to their website. 
+ - Add a teacher.
+ 
+ To-do list:
+ - Create Auth system for users.
+ - Allow users to save teachers.
+ - Allow users to send emails to teachers from app.
+ - Allow users to add events
+## CONTRIBUTORS GUIDE [Joe Gailey](https://github.com/jgaileyiii?tab=repositories) 
+
--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,13 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("Dom Content Loaded")
+})
+
 function openInstrNav() {
     document.getElementById("instrument-content").style.width = "15%";
 }
-
 function openSSNav() {
     document.getElementById("ss-content").style.width = "15%";
 }
-
 function closeInstrNav() {
     document.getElementById("instrument-content").style.width = "0";
 }
@@ -14,6 +16,7 @@ function closeSSNav() {
 }
 
 const $container = document.querySelector('#card-container')
+
 
 fetch(`http://localhost:3000/api/v1/teachers/`)
     .then(response => response.json())
@@ -39,10 +42,8 @@ function createTeacherCards(teachers, event){
     teachers.forEach(teacher => {
         if (teacher.instruments == event.target.innerText){
             const div2 = document.createElement('div')
-            const $container = document.querySelector('#card-container')
             div2.className = 'teacherCard' 
             
-
             div2.innerHTML = 
             `<h1>${teacher.name}</h1>
             <p>${teacher.style}</p>
@@ -51,5 +52,48 @@ function createTeacherCards(teachers, event){
             `
             $container.appendChild(div2)
         }
+    })
+}
+
+function addTeacher () {
+    while ($container.firstChild) {
+        $container.removeChild($container.firstChild)
+    }
+    const teacherForm = document.createElement('form')
+    teacherForm.className = 'teacherForm'
+    
+
+    teacherForm.innerHTML = 
+    `<input id="comment_input" type="text" name="name" placeholder="Name"/>
+    <input id="comment_input" type="text" name="bio" placeholder="Add Bio"/>
+    <input id="comment_input" type="text" name="instruments" placeholder="Instrument(s)"/>
+    <input id="comment_input" type="text" name="style" placeholder="Styles Played"/>
+    <input id="comment_input" type="text" name="url" placeholder="Website"/>
+    <input id="comment_input" type="text" name="phone_number" placeholder="Phone Number"/>
+    <input id="comment_input" type="text" name="email" placeholder="E-Mail"/>
+    <input class="submit" type="submit" value="Submit"/>
+    `
+    $container.appendChild(teacherForm)
+
+    teacherForm.addEventListener('submit', () => {
+        const formData = new FormData(teacherForm)
+        const teacherData = {
+            name: formData.get("name"), 
+            bio: formData.get("bio"), 
+            instruments: formData.get("instruments"), 
+            style: formData.get("style"),
+            url: formData.get("url"), 
+            phone_number: formData.get("phone_number"), 
+            email: formData.get("email")
+          }
+        
+          fetch(`http://localhost:3000/api/v1/teachers/`, {
+            method: 'POST',
+            headers: {
+              "Accept": "application/json",
+              'Content-Type': 'application/json'  
+            },
+            body: JSON.stringify(teacherData)     
+          })
     })
 }
